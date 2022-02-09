52d1e4def85e27128809efa9d12f4555b1577017
danielbarretoithealth <danielebarreto@ucundinamarca.edu.co>
2022-02-09 11:32:20 -0500

    comit 1
    se hace el respectivo commit

---
+++ Peliculas/Crud.netCore-master/CrudNetCore5/Controllers/PeliculasController.cs
@@ -0,0 +1,122 @@
+﻿using CrudNetCore5.Data;
+using CrudNetCore5.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CrudNetCore5.Controllers
+{
+    public class PeliculasController : Controller
+    {
+
+        private readonly ApplicationDbContext _context;
+
+        public PeliculasController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            IEnumerable<Peliculas> listLibros = _context.Peliculas;
+
+            return View(listLibros);
+        }
+
+        //http Get Create 
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        //http Post Create 
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Peliculas Peliculas)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Peliculas.Add(Peliculas);
+                _context.SaveChanges();
+
+                TempData["mensaje"] = "se guardo correctamente";
+                return RedirectToAction("Index");
+            }
+
+            return View();
+        }
+
+        //http Get Edit
+        public IActionResult Edit(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+
+            //obtener el libro 
+            var libro = _context.Peliculas.Find(id);
+            if (libro == null)
+            {
+                return NotFound();
+            }
+
+            return View(libro);
+        }
+
+
+        //http Post Edit 
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(Peliculas Peliculas)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Peliculas.Update(Peliculas);
+                _context.SaveChanges();
+
+                TempData["mensaje"] = "se ha actualizado correctamente";
+                return RedirectToAction("Index");
+            }
+
+            return View();
+        }
+
+        //http Get Delete
+        public IActionResult Delete(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+
+            //obtener el libro 
+            var libro = _context.Peliculas.Find(id);
+            if (libro == null)
+            {
+                return NotFound();
+            }
+
+            return View(libro);
+        }
+        //http Post Delete 
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult DeleteLibro(int? id)
+        {
+            var libro = _context.Peliculas.Find(id);
+            if (libro == null)
+            {
+                return NotFound();
+            }
+             _context.Peliculas.Remove(libro);
+             _context.SaveChanges();
+
+             TempData["mensaje"] = "La pelicula se elimino correctamente";
+             return RedirectToAction("Index");
+        }
+    }
+
+}
---
+++ Peliculas/Crud.netCore-master/CrudNetCore5/Migrations/20220209153756_migracioninicial2.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace CrudNetCore5.Migrations
+{
+    public partial class migracioninicial2 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Libro");
+
+            migrationBuilder.CreateTable(
+                name: "Peliculas",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Titulo = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    Descripcion = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    FechaLanzamiento = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    Autor = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Precio = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Peliculas", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Peliculas");
+
+            migrationBuilder.CreateTable(
+                name: "Libro",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Autor = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Descripcion = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    FechaLanzamiento = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    Precio = table.Column<int>(type: "int", nullable: false),
+                    Titulo = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Libro", x => x.Id);
+                });
+        }
+    }
+}
